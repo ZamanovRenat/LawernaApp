83480ce8cff2082d8293cf640aab58d8a04c3ac5
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-01 19:41:57 +0300

    Добавление библиотеки для работы с сайта АПИ и класса OpenWeatherMapClient для работы с Http клиентом

---
+++ OpenWeatherMap/OpenWeatherMapClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+
+namespace OpenWeatherMap
+{
+    public class OpenWeatherMapClient
+    {
+        private readonly HttpClient _client;
+
+        /// <summary>
+        /// Передача в класс объект класса HttpClient
+        /// </summary>
+        /// <param name="client">Объект класса HttpClient</param>
+        public OpenWeatherMapClient(HttpClient client) => _client = client;
+    }
+}
