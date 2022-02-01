cd43aeca82433fa27b54709bc42a54ad33d8af3c
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-01 20:22:49 +0300

    Метод получения географических координат по названию населенного пункта и класс определения id местоположения

--- OpenWeatherMap/OpenWeatherMapClient.cs
+++ OpenWeatherMap/OpenWeatherMapClient.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace OpenWeatherMap
 {
@@ -11,5 +13,16 @@ namespace OpenWeatherMap
         /// </summary>
         /// <param name="client">Объект класса HttpClient</param>
         public OpenWeatherMapClient(HttpClient client) => _client = client;
+
+        /// <summary>
+        /// Метод получения географических координат по названию населенного пункта
+        /// </summary>
+        /// <param name="Name">Название населенного пункта</param>
+        /// <returns></returns>
+        public async Task<WeatherLocation> GetLocationByName(int id, string Api)
+        {
+            return await _client.GetFromJsonAsync<WeatherLocation>(
+                $"http://api.openweathermap.org/data/2.5/weather?id={id}&lang=ru&appid={Api}");
+        }
     }
 }
---
+++ OpenWeatherMap/WeatherLocation.cs
@@ -0,0 +1,25 @@
+﻿namespace OpenWeatherMap
+{
+    /*
+     * Класс определения id местоположения
+     * Документация по https://openweathermap.org/api/geocoding-api
+     * Пример для города Казань
+     *     {
+        "id": 551487,
+        "name": "Kazan",
+        "state": "",
+        "country": "RU",
+        "coord": {
+            "lon": 49.122139,
+            "lat": 55.788738
+        }
+    },
+     */
+    public class WeatherLocation
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+        public (double lon, double lat) coord { get; set; }
+    }
+}
