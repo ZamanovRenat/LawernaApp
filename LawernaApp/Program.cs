3610e0d1e0191f0521c1c731a1084ec3311abbda
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-01 19:33:23 +0300

    Настройка хостинга

--- LawernaApp/Program.cs
+++ LawernaApp/Program.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace LawernaApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IHost _hosting;
+
+        public static IHost hosting => _hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+        public static IServiceProvider Services => hosting.Services;
+        /// <summary>
+        /// Построитель хоста
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+        /// <summary>
+        /// Метод конфигурирования сервисов
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="services"></param>
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
+            
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            using var host = hosting;
+            await host.StartAsync();
+
+            await host.StopAsync();
+
             string path = @"C:\Programs on C#\LawernaApp\ApiKey.txt";
 
             using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
